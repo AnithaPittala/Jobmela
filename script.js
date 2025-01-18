a795ef8cbf29be802763e18c6005cd757ca4bdbe
AnithaPittala <panithaanitha8311@gmail.com>
2025-01-18 13:33:29 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+// const companySlider = document.querySelector('.company-slider');
+// const prevButton = document.querySelector('.prev-btn');
+// const nextButton = document.querySelector('.next-btn');
+
+// let currentIndex = 0;
+
+// function slideCompanies() {
+//   const offset = -(currentIndex * 250); // Adjust based on card width
+//   companySlider.style.transform = `translateX(${offset}px)`;
+// }
+
+// prevButton.addEventListener('click', () => {
+//   currentIndex = Math.max(0, currentIndex - 1);
+//   slideCompanies();
+// });
+
+// nextButton.addEventListener('click', () => {
+//   const maxIndex = companySlider.childElementCount - 1;
+//   currentIndex = Math.min(maxIndex, currentIndex + 1);
+//   slideCompanies();
+// });
